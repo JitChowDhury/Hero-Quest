8373f7c671176c1c491cc1048e68c696c1525e83
JitChowDhury <jitchowdhury8717@gmail.com>
2025-05-16 22:10:33 +0530

    Understanding main game loop

--- HeroQuest/main.cpp
+++ HeroQuest/main.cpp
@@ -4,6 +4,9 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");//renderwindow class , window is object
   
-
+    while (window.isOpen())
+    {
+        //code
+    }
     return 0;
 } 
