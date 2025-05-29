0d40d5adc4c575998b7657fcacae73649ae88eb0
JitChowDhury <jitchowdhury8717@gmail.com>
2025-05-29 14:29:12 +0530

    Add Enemy Texture

--- HeroQuest/main.cpp
+++ HeroQuest/main.cpp
@@ -7,7 +7,7 @@ int main()
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
 
-    sf::RenderWindow window(sf::VideoMode(800, 600), "RPG Game",sf::Style::Default,settings);//renderwindow class , window is object
+    sf::RenderWindow window(sf::VideoMode(900, 600), "RPG Game",sf::Style::Default,settings);//renderwindow class , window is object
     //sf::CircleShape shape(50.0f);
     //shape.setFillColor(sf::Color::Red);
     //shape.setPosition(sf::Vector2f(100, 100));
@@ -15,8 +15,28 @@ int main()
     
     //--------------------------------INITIALIZE-----------------------------------
     //--------------------------------LOAD-----------------------------------
-    sf::Texture playerTexture;
-    sf::Sprite playerSprite;
+    //--------------------------------ENEMY-----------------------------------
+    sf::Texture enemyTexture;
+    sf::Sprite enemySprite;
+
+    if (enemyTexture.loadFromFile("Assets/Enemy/Textures/Enemy_Idle.png"))
+    {
+        std::cout << "Enemy texture loaded" << std:: endl;
+        enemySprite.setTexture(enemyTexture);
+
+        int XIndex = 0;
+        int YIndex = 0;
+
+        enemySprite.setTextureRect(sf::IntRect(XIndex * 32, YIndex * 32, 32, 32));
+        enemySprite.scale(sf::Vector2f(3, 3));
+        
+    }
+    
+    //--------------------------------ENEMY----------------------------------- 
+    //--------------------------------PLAYER----------------------------------- 
+    
+    sf::Texture playerTexture;//texture is array of pixels
+    sf::Sprite playerSprite;//spirte holds texture
 
     if (playerTexture.loadFromFile("Assets/Player/Textures/Sprite_Up.png"))
     {
@@ -25,13 +45,14 @@ int main()
         //Int
         int XIndex = 0;
         int YIndex = 0;
-        playerSprite.setTextureRect(sf::IntRect(XIndex*64, YIndex*64, 64, 64));//(X,Y,Width,Height)
+        playerSprite.setTextureRect(sf::IntRect(XIndex*64, YIndex*64, 64, 64));//(X,Y,Width,Height)//select a part of the spriteSheet
         playerSprite.scale(sf::Vector2f(3, 3));
     }
     else
     {
         std::cout << "Player Image Failed to Load" << std::endl;
     }
+    //--------------------------------PLAYER----------------------------------- 
     //--------------------------------LOAD-----------------------------------
   
     while (window.isOpen())//everytime we go through we draw one frame so in o  sec it runs 60 time for 60fps
@@ -84,6 +105,7 @@ int main()
         window.clear(sf::Color::Black);//1.clears screen from prev 
 
         window.draw(playerSprite);
+        window.draw(enemySprite);
         window.display(); //swap the back buffer with front
         //---------------------------------DRAW------------------------------------
     }
